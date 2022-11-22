a38b23d03a83e4cd677880f8324c194d804dfee1
Romulo <rhabbiati@hotmail.com>
2022-11-22 06:34:27 +0000

    Repository, projection, DTO, commandLineRunner

--- uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
+++ uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -1,12 +1,37 @@
 package com.devsuperior.uri2602;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Uri2602Application {
+import com.devsuperior.uri2602.dto.CustomerMinDTO;
+import com.devsuperior.uri2602.projections.CustomerMinProjection;
+import com.devsuperior.uri2602.repositories.CustomerRepository;
 
+@SpringBootApplication
+public class Uri2602Application implements CommandLineRunner {
+
+	@Autowired
+	private CustomerRepository customerRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2602Application.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		List<CustomerMinProjection> list = customerRepository.search1("RS");
+		List<CustomerMinDTO> listDTO = list.stream()
+					.map(customerMinProjection -> new CustomerMinDTO(customerMinProjection))
+					.collect(Collectors.toList());
+		
+		for(CustomerMinDTO customerDTO : listDTO) {
+			System.out.println(customerDTO);
+		}
+	}
+
 }
