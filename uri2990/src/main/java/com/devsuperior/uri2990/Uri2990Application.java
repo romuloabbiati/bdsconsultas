3954e2b91fee98eb74a2cd104192a204b464ee93
Romulo <rhabbiati@hotmail.com>
2022-11-27 19:39:06 +0000

    uri2990 Native SQL & JPQL for NOT IN & LEFT JOIN

--- uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
+++ uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
@@ -1,10 +1,15 @@
 package com.devsuperior.uri2990;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 import com.devsuperior.uri2990.repositories.EmpregadoRepository;
 
 @SpringBootApplication
@@ -19,6 +24,36 @@ public class Uri2990Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		// Native SQL
+		List<EmpregadoDeptProjection> list = repository.search1();
+		List<EmpregadoDeptDTO> listDTO = list.stream()
+			.map(empregadoDeptProjection -> new EmpregadoDeptDTO(empregadoDeptProjection))
+			.collect(Collectors.toList());
 		
+		System.out.println("\n*** Native SQL NOT IN");
+		for(EmpregadoDeptDTO empregadoDTO : listDTO) {
+			System.out.println(empregadoDTO);
+		}
+		
+		System.out.println("\n\n");
+		
+		// JPQL
+		List<EmpregadoDeptDTO> listJPQL = repository.search2();
+		
+		System.out.println("\n*** JPQL NOT IN");
+		for(EmpregadoDeptDTO empregadoDTO : listDTO) {
+			System.out.println(empregadoDTO);
+		}
+		
+		// Native SQL LEFT JOIN
+		List<EmpregadoDeptProjection> anotherList = repository.search3();
+		List<EmpregadoDeptDTO> anotherListDTO = anotherList.stream()
+			.map(empregadoDeptProjection -> new EmpregadoDeptDTO(empregadoDeptProjection))
+			.collect(Collectors.toList());
+		
+		System.out.println("\n*** Native SQL LEFT JOIN");
+		for(EmpregadoDeptDTO empregadoDTO : anotherListDTO) {
+			System.out.println(empregadoDTO);
+		}
 	}
 }
--- uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
+++ uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
@@ -1,9 +1,48 @@
 package com.devsuperior.uri2990.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
 import com.devsuperior.uri2990.entities.Empregado;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 
+@Repository
 public interface EmpregadoRepository extends JpaRepository<Empregado, Long> {
 
+	@Query(nativeQuery = true, 
+		   value = "SELECT empregados.cpf, empregados.enome, departamentos.dnome " + 
+		   		"FROM empregados " + 
+		   		"INNER JOIN departamentos ON empregados.dnumero = departamentos.dnumero " + 
+		   		"WHERE empregados.cpf NOT IN ( " + 
+		   		"	SELECT empregados.cpf " + 
+		   		"	FROM empregados " + 
+		   		"	INNER JOIN trabalha ON empregados.cpf = trabalha.cpf_emp " + 
+		   		") " + 
+		   		"ORDER BY empregados.cpf")
+	List<EmpregadoDeptProjection> search1();
+	
+	@Query("SELECT new com.devsuperior.uri2990.dto.EmpregadoDeptDTO(empregado.cpf, empregado.enome, " + 
+			"empregado.departamento.dnome) " + 
+			"FROM Empregado empregado " + 
+			"WHERE empregado.cpf NOT IN ( " + 
+			"	SELECT empregado.cpf " + 
+			"	FROM Empregado empregado " + 
+			"	INNER JOIN empregado.projetosOndeTrabalha " + 
+			") " + 
+			"ORDER BY empregado.cpf")
+	List<EmpregadoDeptDTO> search2();
+	
+	@Query(nativeQuery = true,
+		   value = "SELECT empregados.cpf, empregados.enome, departamentos.dnome " + 
+		   		"FROM empregados " + 
+		   		"INNER JOIN departamentos ON empregados.dnumero = departamentos.dnumero " + 
+		   		"LEFT JOIN trabalha ON empregados.cpf = trabalha.cpf_emp " + 
+		   		"WHERE trabalha.cpf_emp IS NULL " + 
+		   		"ORDER BY empregados.cpf")
+	List<EmpregadoDeptProjection> search3();
+	
 }
